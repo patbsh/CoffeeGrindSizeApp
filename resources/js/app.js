e66cba841ee6fe2d2d8af78ee23611cc5844bf22
Solpadeine <patryk@gustomania.pl>
2022-09-12 20:01:20 +0200

    remove test component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,10 @@ window.Form = Form;
 import Form from './form';
 import { createApp } from 'vue/dist/vue.esm-bundler';
 
-import TestComponent from './components/TestComponent.vue';
 import GrindSizeComponent from './components/GrindSizeComponent.vue';
 
 const app = createApp({});
 
-app.component('test-component', TestComponent);
 app.component('grind-size-component', GrindSizeComponent);
 
 app.mount("#app");
