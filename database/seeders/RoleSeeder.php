219d86bc55cb454424e98c9ca06ecfe323b2d542
Solpadeine <patryk@gustomania.pl>
2022-11-10 20:22:52 +0100

    install spatie permissions, create roles, create seeders for role and user

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            'Admin',
+            'User',
+        ];
+        foreach ($roles as $role) {
+            Role::create([
+                'name'       => $role,
+                'guard_name' => 'web'
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' => Str::random(10)
+        ]);
+        $admin->assignRole(['Admin']);
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'remember_token' => Str::random(10)
+        ]);
+        $user->assignRole(['User']);
+    }
+}
