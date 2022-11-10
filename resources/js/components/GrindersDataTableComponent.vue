97e36b91267f1a7d0b443794879a5047fb156009
Solpadeine <patryk@gustomania.pl>
2022-11-10 16:34:20 +0100

    Grinder table using normal table component (datatable component is not yet supported by vuetify)

--- resources/js/components/GrindersDataTableComponent.vue
+++ resources/js/components/GrindersDataTableComponent.vue
@@ -1,10 +1,46 @@
 <template>
-
+    <v-table>
+        <thead>
+        <tr>
+            <th class="text-left">
+                Grinder producer
+            </th>
+            <th class="text-left">
+                Grinder model
+            </th>
+            <th class="text-left">
+                Details
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="grinder in grinders"
+            :key="grinder.model"
+        >
+            <td>
+                {{ grinder.grinder_producer.name }}
+            </td>
+            <td>
+                {{ grinder.model }}
+            </td>
+            <td>
+                <v-btn
+                    color="primary"
+                    rounder="lg"
+                    :href="this.grinder_link + '/' + grinder.id"
+                >
+                    Details
+                </v-btn>
+            </td>
+        </tr>
+        </tbody>
+    </v-table>
 </template>
 <script>
 export default {
     components: {},
-    props: ["grinders"],
+    props: ["grinders","grinder_link"],
     data() {
         return {
         }
